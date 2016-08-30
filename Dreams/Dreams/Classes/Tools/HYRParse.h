27130202a7611fe31d07188100f13b437c80b6a1
tarena <2507233893@qq.com>
2016-08-30 18:20:09 +0800

    刚刚修改错了，修改了测试里面的info.plist文件里面的了。应该是修改项目下的info.plist文件里面的

---
+++ Dreams/Dreams/Classes/Tools/HYRParse.h
@@ -0,0 +1,17 @@
+//
+//  HYRParse.h
+//  Dreams
+//
+//  Created by aiqiyisheng on 16/8/30.
+//  Copyright © 2016年 黄永锐. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HYRParse : NSObject
+
+//解析 梦的类型
++(NSMutableArray*)parseDreamsTypeWithDic:(NSDictionary *)dict;
+
+
+@end
---
+++ Dreams/Dreams/Classes/Tools/HYRParse.m
@@ -0,0 +1,21 @@
+//
+//  HYRParse.m
+//  Dreams
+//
+//  Created by aiqiyisheng on 16/8/30.
+//  Copyright © 2016年 黄永锐. All rights reserved.
+//
+
+#import "HYRParse.h"
+
+@implementation HYRParse
+//解析 梦的类型
++(NSMutableArray*)parseDreamsTypeWithDic:(NSDictionary *)dict{
+    NSMutableArray * array = [NSMutableArray array];
+    
+    
+    
+    return array;
+}
+
+@end
